5b7912ae20f0b52bb82a7dd13a9ff7c555f13db9
saleem <ab588538@gmail.com>
2026-06-01 16:04:24 +0530

    add the add and edit option

--- src/app/employee/employee-add/employee-add.ts
+++ src/app/employee/employee-add/employee-add.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-employee-add',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-add.html',
   styleUrl: './employee-add.css',
 })
-export class EmployeeAdd {}
+export class EmployeeAdd {
+
+  employee = {
+    name: '',
+    position: '',
+  };
+
+  saveEmployee() {
+    console.log('Employee saved:', this.employee);
+    alert(`Employee saved: ${this.employee.name} - ${this.employee.position}`);
+  }
+}
--- src/app/employee/employee-list/employee-list.ts
+++ src/app/employee/employee-list/employee-list.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-employee-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.css',
 })
-export class EmployeeList {}
+export class EmployeeList {
+  employees = [
+    { id: 1, name: 'John Doe', position: 'Software Engineer' },
+    { id: 2, name: 'Jane Smith', position: 'Product Manager' },
+    { id: 3, name: 'Alice Johnson', position: 'UX Designer' },
+  ];
+
+  editEmployee(employee:any){
+    const newName = prompt('Enter new name:', employee.name);
+    const newPosition = prompt('Enter new position:', employee.position);
+    if (newName){
+      employee.name = newName;
+    }
+    if (newPosition){
+      employee.position = newPosition;
+    }
+  }
+
+    deleteEmployee(id: number) {
+    this.employees = this.employees.filter(
+      employee => employee.id !== id
+    );
+  }
+
+  addEmployee() {
+    const name = prompt('Enter employee name:');
+    const position = prompt('Enter employee position:');
+    if (name && position) {
+      const newEmployee = {
+        id: this.employees.length + 1,
+        name,
+        position
+      };
+      this.employees.push(newEmployee);
+    }
+  }
+}
